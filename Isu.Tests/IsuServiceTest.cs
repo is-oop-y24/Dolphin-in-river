15465dd8f42d5ce62684f4389f1dc2ada5b5e5ec
Dolphin-in-river <311508@niuitmo.ru>
2021-09-18 20:29:37 +0300

    fix: change Group Names

--- Isu.Tests/IsuServiceTest.cs
+++ Isu.Tests/IsuServiceTest.cs
@@ -19,7 +19,7 @@ namespace Isu.Tests
         public void AddStudentToGroup_StudentHasGroupAndGroupContainsStudent()
         {
             Setup();
-            Group group = _isuService.AddGroup("m3101");
+            Group group = _isuService.AddGroup("M3101");
             Student student = _isuService.AddStudent(group, "Ivan");
             if ((group.GetStudent(-1, "Ivan") != null) && (student.GetGroup().Equals(group.GetName())))
             {
@@ -34,7 +34,7 @@ namespace Isu.Tests
             Assert.Catch<IsuException>(() =>
             {
                 var isu = new IsuService();
-                Group group = isu.AddGroup("m3203");
+                Group group = isu.AddGroup("M3203");
                 for (int i = 0; i < 21; i++)
                 {
                     var student = new Student($"Student {i}", group.GetName());
@@ -57,8 +57,8 @@ namespace Isu.Tests
         public void TransferStudentToAnotherGroup_GroupChanged()
         {
             Setup();
-            Group groupFirst = _isuService.AddGroup("m3101");
-            Group groupSecond = _isuService.AddGroup("m3102");
+            Group groupFirst = _isuService.AddGroup("M3101");
+            Group groupSecond = _isuService.AddGroup("M3102");
             Student student = _isuService.AddStudent(groupFirst, "Ivan"); 
             _isuService.ChangeStudentGroup(student, groupSecond);
             if (groupFirst.GetStudent(-1, "Ivan") == null && groupSecond.GetStudent(-1, "Ivan") != null )
