7ae217cad7046aa36b01a8d9986c4ac8f41349a6
Dolphin-in-river <311508@niuitmo.ru>
2021-10-29 11:33:35 +0300

    feat: add test for local system.

--- Backups/Program.cs
+++ Backups/Program.cs
@@ -1,9 +1,34 @@
-﻿namespace Backups
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Backups.Tools;
+
+namespace Backups
 {
     internal class Program
     {
         private static void Main()
         {
+            string directory1 = "C:/Users/Иван/Desktop/1.txt";
+            string directory2 = "C:/Users/Иван/Desktop/2.txt";
+            var listFile = new List<string>
+            {
+                directory1,
+                directory2,
+            };
+
+            bool localKeep = true;
+            var backupJob = new BackupJob(listFile, "Split storage", "C:/Users/Иван/Desktop/", localKeep);
+
+            backupJob.CreateRestorePoint();
+
+            backupJob.DeleteFile(directory1);
+
+            backupJob.CreateRestorePoint();
+            if (backupJob.AmountPoints() != 2 && backupJob.AmountStorage() != 3)
+            {
+                throw new BackupsException("Error with creating a backup");
+            }
         }
     }
 }
