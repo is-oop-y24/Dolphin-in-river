c723c9544d80117df8484bbb57ea88a54d02e437
Dolphin-in-river <311508@niuitmo.ru>
2021-12-03 19:04:42 +0300

    feat: wrappers for entities from the past lab

---
+++ BackupsExtra/RestorePointWithDateCreation.cs
@@ -0,0 +1,68 @@
+using System;
+using System.IO;
+using Backups;
+using BackupsExtra.Logging;
+
+namespace BackupsExtra
+{
+    public class RestorePointWithDateCreation
+    {
+        public RestorePointWithDateCreation(IRestorePoint point, AbstractClassLogging logging)
+        {
+            Point = point;
+            Logging = logging;
+            NewRepository = new RepositoryExtra(logging);
+        }
+
+        public IRestorePoint Point
+        {
+            get;
+            set;
+        }
+
+        public AbstractClassLogging Logging
+        {
+            get;
+            set;
+        }
+
+        public RepositoryExtra NewRepository
+        {
+            get;
+            set;
+        }
+
+        public void RecoverSplitPoint(IRecovery recoveryInfo)
+        {
+            NewRepository.RecoverSplitPoint(recoveryInfo, Point);
+        }
+
+        public void RecoverSinglePoint(IRecovery recoveryInfo)
+        {
+            NewRepository.RecoverSinglePoint(recoveryInfo, Point);
+        }
+
+        public void ExecuteLoggingCreationPoint()
+        {
+            string text = null;
+            if (Logging.Configuration)
+            {
+                text += Point.CreateDataTime + ", ";
+            }
+
+            text += Point.GetType() + " has been created" + Environment.NewLine;
+            if (Logging.TypeLogging.Equals(TypeLogging.ConsoleLogging))
+            {
+                Console.WriteLine(text);
+            }
+            else
+            {
+                var loggingToFile = (FileLogging)Logging;
+                var fileStream = new FileStream(loggingToFile.FilePath, FileMode.Append);
+                byte[] array = System.Text.Encoding.Default.GetBytes(text);
+                fileStream.Write(array, 0, array.Length);
+                fileStream.Dispose();
+            }
+        }
+    }
+}
